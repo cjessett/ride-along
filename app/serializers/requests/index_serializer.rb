d014abc30cfa7db31bdcd5306c24d51c8c47ba20
CJ Jessett <cjessett@gmail.com>
2016-09-14 18:43:14 -0500

    conflic

--- app/serializers/requests/index_serializer.rb
+++
@@ -1,3 +0,0 @@
-class Requests::IndexSerializer < RequestSerializer
-  belongs_to :user
-end
--- app/serializers/trip_serializer.rb
+++
@@ -1,9 +0,0 @@
-class TripSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :departure, :arrival, :driver, :email, :rating, :count, :riders
-=======
-  attributes :id, :departure, :arrival
->>>>>>> 4545c1534ea1b8ea442259e941b640b4b48c8d5f
-
-
-end
